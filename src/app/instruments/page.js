c334b46a5bfffbdd18de4fec64309c7c43cd837a
chenry34 <christianhenry34509@gmail.com>
2023-09-26 20:04:42 -0500

    Try to fix font

--- src/app/instruments/page.js
+++ src/app/instruments/page.js
@@ -2,7 +2,7 @@ export default function instruments() {
   return ( 
     <>
       <div className="container">
-        <div className="row mt-5 mb-3">
+        <div className="row mt-3 mb-3">
           <div className="col">
             <p style={{fontSize: "20px"}}>Clients will be granted access to the Skedda online booking system following the completion of the instrument training provided by the QuIPS Platform technicians.</p>
             <h3 className="mt-3">Please click below to book the instruments!</h3>
